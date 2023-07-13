6e9c12dc060f5512e7c2d44b8f34e03183d93ade
Nursultan <User@LAPTOP-P5M6HURA>
2023-07-13 14:34:17 +0600

    Go1

---
+++ Buzzkill.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!1" << endl;
+	cout << "Hello World!2" << endl;
+	cout << "Hello World!3" << endl;
+	cout << "Hello World!4" << endl;
+	cout << "Hello World!5" << endl;
+	cout << "Hello World!6" << endl;
+	cout << "Hello World!7" << endl;
+	cout << "Hello World!8" << endl;
+	cout << "Hello World!9" << endl;
+	cout << "Hello World!10\n";
+	cout.flush();
+	return 0;
+}
