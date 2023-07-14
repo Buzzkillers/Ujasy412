3b4c8db85afeb2d988d275ada98c27e86da2ce54
Nursultan <User@LAPTOP-P5M6HURA>
2023-07-14 11:46:25 +0600

    save1

--- Buzzkill.cpp
+++ Buzzkill.cpp
@@ -1,19 +1,77 @@
 #include<iostream>
+#include<string>
+#include <vector>
+#include <algorithm>
+
+/// <summary>
+/// int iNameSpare
+/// Object fName
+/// double dName
+/// bool bIsEmpty
+/// vector<int> arrName
+/// * ptrName
+/// </summary>
 
 using namespace std;
+unsigned int a = 5ll;
+const float b(0.43f);
+double c = 2.564;
 
-int main()
-{
-	cout << "Hello World!1" << endl;
-	cout << "Hello World!2" << endl;
-	cout << "Hello World!3" << endl;
-	cout << "Hello World!4" << endl;
-	cout << "Hello World!5" << endl;
-	cout << "Hello World!6" << endl;
-	cout << "Hello World!7" << endl;
-	cout << "Hello World!8" << endl;
-	cout << "Hello World!9" << endl;
-	cout << "Hello World!10\n";
+
+
+__int64 ill;
+
+bool f;
+int main() {
+
+	wchar_t s = L'9';
+
+	double dCoolParams = 413;
+	double dCoolParams2 = 413e-2;
+	double dCoolParams3 = 413e3;
+	long long ll(0);
+
+	vector < vector<int>>  arrMul(100, vector<int>(100));
+
+	vector<int> arrT = { 7, 8, 5, 90 };
+
+
+	reverse(arrT.begin(), arrT.end());
+
+
+
+	for (int i = 30; i < 2000; ++i)
+	{
+		wstring w;
+		w.push_back(wchar_t(3434));
+
+
+		wcout << w;
+	}
+
+	arrT.push_back
+
+
+	(0
+
+
+	)
+
+
+
+		;
+
+
+
+	cout << "Pank hou!2" << endl;
+	cout << "Pank hou!3" << endl;
+	cout << "Pank hou!4" << endl;
+	cout << "Pank hou!5" << endl;
+	cout << "Pank hou!6" << endl;
+	cout << "Pank hou!7" << endl;
+	cout << "Pank hou!8" << endl;
+	cout << "Pank hou!9" << endl;
+	cout << "Pank hou!10\n";
 	cout.flush();
 	return 0;
 }
