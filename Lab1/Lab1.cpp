4ab1f969622e1f3a8f87133c1ed5a3b6d62cbfd9
student <student@ULK413W10X64C14>
2023-07-14 13:18:11 +0700

    check

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,9 +1,21 @@
 ﻿#include <iostream>
-using namespace std;
+#include <string>
+#include <vector>
+#include <algorithm>
 
-int main()
-{
-	cout << "Hello World!\n";
-	cout << "Hello World!\n";
-	cout << "Hello World!\n";
+using namespace std;
+int iNameName = 0;
+wchar_t m = L'^';
+
+vector<vector<int>> arrNum =
+	{ 7, 8, 90, 100 };
+
+arrNum.push_back(98);
+arrNum.push_back(98);
+arrNum.pop_back();
+
+int main() {
+	double dNeedName = 3.1415;
+	cout << "Hello World!\n"; //Ctrl+D Дублируем строку
+	cout << dNeedName;
 }
